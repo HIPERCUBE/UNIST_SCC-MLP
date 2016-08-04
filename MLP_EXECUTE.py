2c673c2ce0e5f3fcc8d125a29ef52cfce980f8f6
Seungwoo Yeom <ysw0094@gmail.com>
2016-08-04 15:57:37 +0900

    Define experiment function

--- MLP_EXECUTE.py
+++ MLP_EXECUTE.py
@@ -1,15 +1,31 @@
-import os
+import os, sys
 
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 
-commandList = [
-    "cd " + CURRENT_PATH,
-    "ls"
-    "./mlp.x sample.in 50 30 10 1000 1"
-]
 
-for command in commandList:
-    mlpOpen = os.popen(command)
-    mlpRead = mlpOpen.read()
-    print(command)
-    print(mlpRead)
+def experiment(trainingPer, testingPer, neuron, epochs, runningMate, outputRate):
+    commandList = [
+        "cd " + CURRENT_PATH,
+        "ls",
+        # "./mlp.x sample.in 1 60 90 10 10 1 10 0"
+        "./mlp.x sample.in {} {} {} {} {} {}".format(trainingPer, testingPer, neuron,
+                                                     epochs, runningMate, outputRate)
+    ]
+
+    for command in commandList:
+        mlpOpen = os.popen(command)
+        mlpRead = mlpOpen.read()
+        print(command)
+        print(mlpRead)
+        if command == commandList[-1]:
+            fileResult = mlpRead.split('<RESULT FLAG>')[1]
+            print(fileResult)
+            filePath = CURRENT_PATH + "/{} {} {} {} {} {}.txt".format(trainingPer, testingPer, neuron,
+                                                                      epochs, runningMate, outputRate)
+            print(filePath)
+            file = open(filePath, 'w')
+            file.write(fileResult)
+            file.close()
+
+
+experiment(50, 40, 1, 100, 1, 1)
